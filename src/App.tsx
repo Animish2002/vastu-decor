8a6ead9e6a61b9f3f6342066156e824476b397ad
Animish2002 <animishchopade123@gmail.com>
2025-06-03 11:15:29 +0530

    removed analtics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./LandingPage/Home";
 import Gallery from "./LandingPage/Gallery";
-import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
-      <Analytics />
     </BrowserRouter>
   );
 }
