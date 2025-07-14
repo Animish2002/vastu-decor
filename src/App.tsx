0ff7d0b848fe8f1a9e960f4e5cd4a08a9c0a341d
Animish2002 <animishchopade123@gmail.com>
2025-07-14 10:58:40 +0530

    scroll issue fix from landing page to gallery

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { Analytics } from "@vercel/analytics/react"; // 👈 import Analytics
 
 import Home from "./LandingPage/Home";
 import Gallery from "./LandingPage/Gallery";
+import ScrollToTop from "./LandingPage/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
-      <Analytics /> 
+      <Analytics />
     </BrowserRouter>
   );
 }
